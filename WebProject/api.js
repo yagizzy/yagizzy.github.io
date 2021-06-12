d8dc4b21a033cd426cfe3df32304acdad50047c5
YagizYarogluHW10 <66210577+YagizYarogluHW10@users.noreply.github.com>
2021-06-12 21:02:16 +0300

    re

--- WebProject/api.js
+++ WebProject/api.js
@@ -1,30 +1,14 @@
 
-//$(document).ready(function(){
-// var url =
- // "https://openapi.izmir.bel.tr/api/ibb/kultursanat/etkinlikler/4345" 
- // $.getJSON(url, function(data) {
- // var html = "";
- // $.each(data, function(){
- //  html = "<h2>" + data.Adi+ "</h2>";
- //  html += "<img src=" + data.Resim + ">";
- //  html += "<h2>" + data.Aciklama+ "</h2>";
- // });
- // $("#listele").html(html);
-// });
- // });
- 
- 
- $.ajax({
-            url:"https://openapi.izmir.bel.tr/api/ibb/kultursanat/etkinlikler/4345",
-            dataType:"json",
-            async:false,
-            type:"GET",
-            success:function (data){
-                for (var i=1;i<4;i++){
-                    $("#listele"+i+"").append(data[i]);
-                }
-            },
-            error(){
-                console.log("error");
-            }
-        });
+$(document).ready(function(){
+ var url =
+ "https://openapi.izmir.bel.tr/api/ibb/kultursanat/etkinlikler/4345" 
+ $.getJSON(url, function(data) {
+ var html = "";
+ $.each(data, function(){
+  html = "<h2>" + data.Adi+ "</h2>";
+  html += "<img src=" + data.Resim + ">";
+  html += "<h2>" + data.Aciklama+ "</h2>";
+ });
+ $("#listele").html(html);
+ });
+ });
